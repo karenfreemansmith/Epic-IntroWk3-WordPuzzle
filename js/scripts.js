3a19be1568e10e09f31e1a7540d5de2d829c2209
Karen Freeman-Smith and Katy Henning <katy@>
2016-08-16 13:38:36 -0700

    replace vowels with underscore

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
-  $("form").submit(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
     var phrase = $("#puzzlephrase").val();
-    alert(phrase);
+
+    var letters = phrase.split("");
+
+    for (var index= 0; index<letters.length; index += 1) {
+      if (letters[index] === "a" || letters[index] === "e" || letters[index] === "i" || letters[index] === "o" ||letters[index] === "u"){
+        letters[index] = "_";
+      }
+    }
+      phrase = letters.join("");
+    $("#result").html("<h3>" + phrase + "</h3>");
   });
 });
